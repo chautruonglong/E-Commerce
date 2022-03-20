652908c6f3c60105e004175e8833af5658787c7a
chautruonglong <truonglongchau@gmail.com>
2022-03-20 22:54:07 +0700

    Add database script

---
+++ database.sql
@@ -0,0 +1,42 @@
+create EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table customers (
+   id uuid default uuid_generate_v4() not null,
+    createdAt timestamp default now(),
+    updatedAt timestamp default now(),
+    address text,
+    code text,
+    email text,
+    password text,
+    phone text,
+    primary key (id)
+);
+
+create table orders (
+   id uuid default uuid_generate_v4() not null,
+    createdAt timestamp default now(),
+    updatedAt timestamp default now(),
+    customerId uuid,
+    productId uuid,
+    primary key (id)
+);
+
+create table products (
+   id uuid default uuid_generate_v4() not null,
+    createdAt timestamp default now(),
+    updatedAt timestamp default now(),
+    code text,
+    name text,
+    price double,
+    primary key (id)
+);
+
+create table products_images (
+   products_id uuid default uuid_generate_v4() not null,
+    images text[]
+);
+
+alter table products_images
+   add constraint FKbp8jstfr20o4dtau7wgu5khju
+   foreign key (products_id)
+   references products;
